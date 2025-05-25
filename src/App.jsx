3bed8ac262c1942fc5dd4ca253ddb5447af13342
UmarAalma <umerrazzaq.dev@gmail.com>
2025-05-25 14:28:11 +0500

    Sign-in-page-completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import {
 } from "react-router";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
 
 const App = () => {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<SignUpPage />} />)
+    createRoutesFromElements(<Route path="/" element={<SignInPage />} />)
   );
 
   return <RouterProvider router={router} />;
--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
-const SignUpPage = () => {
+const SignInPage = () => {
   return (
     <div className="flex bg-white h-screen justify-center p-1">
       <div className="rounded-2xl bg-white my-auto white shadow-gray-200 inset-shadow-2xs shadow-lg h-[64%] w-[26%] font-black">
         <h2 className="flex justify-center text-gray-700 text-4xl mt-7">
-          Sign up
+          Sign in
         </h2>
         <form>
           <input
@@ -33,11 +33,11 @@ const SignUpPage = () => {
             type="submit"
             className="cursor-pointer hover:bg-blue-300 text-white rounded-md flex items-center justify-center mx-auto mt-7 bg-blue-400 text-xl h-12 w-[80%]"
           >
-            Sign Up
+            Sign in
           </button>
           <div className="flex justify-center mt-2 font-medium text-gray-400">
-            Already have an account?
-            <a className="cursor-pointer ml-1 text-blue-400">Login</a>
+            Don't have account?
+            <a className="cursor-pointer ml-1 text-blue-400">Sign up</a>
           </div>
         </form>
         <div className="flex justify-between mx-9 mt-5">
@@ -50,11 +50,11 @@ const SignUpPage = () => {
           className="hover:bg-gray-50 outline-2 outline-gray-700 cursor-pointer text-gray-700 rounded-md flex items-center justify-center mx-auto mt-7 bg-white-400 text-xl h-12 w-[80%]"
         >
           <FcGoogle className="mr-2" size={24} />
-          Sign up with Google
+          Sign in with Google
         </button>
       </div>
     </div>
   );
 };
 
-export default SignUpPage;
+export default SignInPage;
