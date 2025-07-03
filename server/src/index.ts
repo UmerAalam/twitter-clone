2e551758bc1dbcebc1f07ac70d602d546a631dd5
UmerAalam <umerrazzaq.dev@gmail.com>
2025-07-03 21:29:07 +0500

    done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,32 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-
+import { Client } from "pg";
 const app = new Hono();
 
+const con = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "password",
+  database: "users",
+});
+con.connect().then(() => console.log("connected"));
+con.query(
+  "INSERT INTO demotable (name,id) VALUES ('razzaq',2)",
+  [],
+  (err, res) => {}
+);
+con.query("Select * from demotable", (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
+  con.end;
+});
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
-
 serve(
   {
     fetch: app.fetch,
