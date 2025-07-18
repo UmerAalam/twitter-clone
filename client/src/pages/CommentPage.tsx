210d678b4c07b6581e6558897d15f7c24d8d842e
UmerAalam <umerrazzaq.dev@gmail.com>
2025-07-18 09:48:40 +0500

    fetching tweet data done

--- client/src/pages/CommentPage.tsx
+++ client/src/pages/CommentPage.tsx
@@ -1,13 +1,25 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 import { client } from "../lib/client";
 import { useParams } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 export const tweetDetailQueryOptions = (id: number) => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  if (!token) {
+    navigate("/sign-in");
+  }
   return queryOptions({
     queryFn: async () => {
-      const res = await client.api.tweets[":id"].$get({
-        param: { id: String(id) },
-      });
+      const res = await client.api.tweets[":id"].$get(
+        {
+          param: { id: String(id) },
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
       const data = await res.json();
       return data;
     },
@@ -21,7 +33,15 @@ const CommentPage = () => {
   const { isLoading, data } = useQuery(
     tweetDetailQueryOptions(parseInt(params.id!)),
   );
-  return <div>{JSON.stringify(data, null, 4)}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <div>Loading Tweet Data</div>
+      ) : (
+        JSON.stringify(data, null, 4)
+      )}
+    </div>
+  );
 };
 
 export default CommentPage;
